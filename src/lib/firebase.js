1f8d726c80deba36cb85031a9954dfffc87ad59e
Pavlos Saba <pavlossaba@gmail.com>
2023-12-18 20:19:38 +0200

    Variables moved into .env file. Responsiveness to be done

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,7 @@
 /* eslint-disable */
 
 import { initializeApp } from "firebase/app";
-import {
-  FieldValue,
-  getFirestore,
-  setDoc,
-  doc,
-  
-} from "firebase/firestore";
+import { FieldValue, getFirestore, setDoc, doc } from "firebase/firestore";
 import "firebase/firestore";
 import {
   getAuth,
@@ -18,18 +12,12 @@ import {
 } from "firebase/auth";
 
 const config = {
-  //   apiKey: process.env.REACT_APP_API_KEY,
-  //   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  //   projectId: process.env.REACT_APP_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  //   appId: process.env.REACT_APP_APP_ID
-  apiKey: "AIzaSyDs1tD8857hcLhT33jVtKRTohru7BJlm8Y",
-  authDomain: "instagram-clone-2023-1c056.firebaseapp.com",
-  projectId: "instagram-clone-2023-1c056",
-  storageBucket: "instagram-clone-2023-1c056.appspot.com",
-  messagingSenderId: "843936159564",
-  appId: "1:843936159564:web:68b60402e949da75cda1e9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebase = initializeApp(config);
@@ -46,5 +34,5 @@ export {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   updateProfile,
-  auth
+  auth,
 };
